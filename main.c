ed0d07b2b88a204eacad5c32283fa88017fb5751
Mykola Karpets <mykola.karpets@globallogic.com>
2017-08-23 13:55:56 +0300

    Working on printf_data_hex
    Signed-off-by: Mykola Karpets <mykola.karpets@globallogic.com>

--- main.c
+++ main.c
@@ -11,22 +11,49 @@
 #include <net/ethernet.h>
 #include <arpa/inet.h>
 
+#define BIG_BUFSIZE 10000
 #define DEF_BUFSIZE 256
 #define DEF_PKTBUFSIZE 2000
+#define BYTES_IN_ROW 16
 
 void die(const char * str, int _errno);
 
+/* Print data, pointed by @data into the @buf as hex values (i.e. 00 FF 00 ...)
+ * 16 bytes in a row, separated by a space.
+ *
+ * If @bufsize < @size - data will be truncated.
+ * @return the same value as buf if succedes, NULL otherwise
+ */
 char * printf_data_hex(char * buf,
 		const unsigned int bufsize,
 		const void * data,
 		const unsigned int size);
 
-void printf_packet(const void * data,
+/* Prints packet, received by a level 2 socket if the following format:
+ *
+ *				Destination MAC: XX:XX:XX:XX:XX:XX
+ *					 Source MAC: XX:XX:XX:XX:XX:XX
+ *				------------------------------------------------
+ *				XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX
+ *				................................................
+ *				XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX XX
+ *				------------------------------------------------
+ *
+ * @buf - destination address
+ * @bufsize - size of memory, allocated by a pointer @buf
+ * @data - packet data
+ * @size - size of data
+ * @_sockaddr_ll - argument of @recvfrom() function.
+ */
+char * printf_packet(char * buf,
+		const unsigned int bufsize,
+		const void * data,
 		const unsigned int size,
 		const struct sockaddr_ll * _sockaddr_ll);
 
 int main(const int argc, const char * argv[]) {
 	char buffer[DEF_PKTBUFSIZE] = {0};
+	char print_buffer[BIG_BUFSIZE] = {0};
 
 	int ethernet_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 	if(ethernet_socket == -1) {
@@ -55,7 +82,11 @@ int main(const int argc, const char * argv[]) {
 				src_addrll.sll_addr[4],
 				src_addrll.sll_addr[5]);
 		printf("--------------------------------------\n");
-		printf_data_hex((void*)buffer,bytes_received);
+		printf_data_hex(print_buffer,
+						DEF_BUFSIZE,
+						(void*)buffer,
+						bytes_received);
+		//printf("%s\n",print_buffer);
 		printf("--------------------------------------\n");
 	}
 
@@ -69,18 +100,42 @@ void die(const char * str, int _errno) {
 	exit(1);
 }
 
-void printf_data_hex(const void * data, const unsigned int size) {
+char * printf_data_hex(char * buf,
+		const unsigned int bufsize,
+		const void * data,
+		const unsigned int size) {
+	char local_buffer[DEF_BUFSIZE] = {0};
+	memset(buf,0,bufsize);
+
 	unsigned int i = 0;
-	unsigned char * _data = (unsigned char * )(data);
-	for(;i<size;i++) {
-		if(i % 16 == 0) printf("\n");
-		printf("%02X ",_data[i]);
+	unsigned int bufsize_left = bufsize;
+	unsigned char * _data = (unsigned char *)(data);
+
+	for(;((i < size) && (bufsize_left > 0));i++) {
+		if(((i % BYTES_IN_ROW) == 0) && (i != 0))
+			strncat(buf,"\n",bufsize_left);
+
+		delta = snprintf(place_to_write,
+				bufsize_left,
+				"%02X ",
+				_data[i]) - 1; //-1 due to printing '\0'
+
+		if(delta > bufsize_left) {
+			fprintf(stdout,"Not enoght size in buffer!\n");
+			return NULL;
+		} else {
+			place_to_write += delta;
+			bufsize_left -= delta;
+		}
 	}
-	printf("\n");
+
+	return buf;
 }
 
-void printf_packet(const void * data,
+char * printf_packet(char * buf,
+		const unsigned int bufsize,
+		const void * data,
 		const unsigned int size,
 		const struct sockaddr_ll * _sockaddr_ll) {
-		
+	return NULL;
 }
